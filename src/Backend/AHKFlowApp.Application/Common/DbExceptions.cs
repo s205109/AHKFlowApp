864e57c0eefe77ffee40a04b8eb0747b706b4464
Bart Segers (M16) <btasegers@live.nl>
2026-05-23 07:57:43 +0200

    refactor: tighten minor review polish

--- src/Backend/AHKFlowApp.Application/Common/DbExceptions.cs
+++ src/Backend/AHKFlowApp.Application/Common/DbExceptions.cs
@@ -8,7 +8,7 @@ internal static class DbExceptions
     // Checks SQL Server unique-constraint error codes (2601/2627) without importing Microsoft.Data.SqlClient,
     // which would couple the Application layer to an infrastructure concern.
     [ExcludeFromCodeCoverage]
-    public static bool IsDuplicateKeyViolation(this DbUpdateException ex) =>
+    internal static bool IsDuplicateKeyViolation(this DbUpdateException ex) =>
         ex.InnerException?.GetType().GetProperty("Number")?.GetValue(ex.InnerException) is int n &&
         n is 2601 or 2627;
 }
