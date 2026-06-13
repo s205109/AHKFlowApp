f5c0bcb2fca5d5ad5539582e1093c4b8fd8d7ea5
Bart Segers (M16) <btasegers@live.nl>
2026-06-13 16:10:56 +0200

    fix: skip stale links in cleanup script

--- delete-bin-and-obj-folders.ps1
+++ delete-bin-and-obj-folders.ps1
@@ -1,1 +1,23 @@
-Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
+$ErrorActionPreference = 'Stop'
+
+$artifactDirectoryNames = @('bin', 'obj')
+$directoriesToVisit = [System.Collections.Generic.Stack[string]]::new()
+$directoriesToVisit.Push((Get-Location).ProviderPath)
+
+while ($directoriesToVisit.Count -gt 0) {
+    $currentDirectory = $directoriesToVisit.Pop()
+    $childDirectories = Get-ChildItem -LiteralPath $currentDirectory -Directory -Force -ErrorAction SilentlyContinue
+
+    foreach ($childDirectory in $childDirectories) {
+        if (($childDirectory.Attributes -band [System.IO.FileAttributes]::ReparsePoint) -ne 0) {
+            continue
+        }
+
+        if ($artifactDirectoryNames -contains $childDirectory.Name) {
+            Remove-Item -LiteralPath $childDirectory.FullName -Force -Recurse
+            continue
+        }
+
+        $directoriesToVisit.Push($childDirectory.FullName)
+    }
+}
