e8e7ee1a89685164353c506651c3a1bf048f6fa9
Bart Segers (M16) <btasegers@live.nl>
2026-06-13 16:17:05 +0200

    chore: show cleanup script progress

--- delete-bin-and-obj-folders.ps1
+++ delete-bin-and-obj-folders.ps1
@@ -3,9 +3,19 @@ $ErrorActionPreference = 'Stop'
 $artifactDirectoryNames = @('bin', 'obj')
 $directoriesToVisit = [System.Collections.Generic.Stack[string]]::new()
 $directoriesToVisit.Push((Get-Location).ProviderPath)
+$scannedDirectoryCount = 0
+$deletedDirectoryCount = 0
+
+Write-Host 'Scanning for bin/obj folders...'
 
 while ($directoriesToVisit.Count -gt 0) {
     $currentDirectory = $directoriesToVisit.Pop()
+    $scannedDirectoryCount++
+
+    if ($scannedDirectoryCount % 100 -eq 0) {
+        Write-Host ("Scanned {0} directories; deleted {1} artifact folders..." -f $scannedDirectoryCount, $deletedDirectoryCount)
+    }
+
     $childDirectories = Get-ChildItem -LiteralPath $currentDirectory -Directory -Force -ErrorAction SilentlyContinue
 
     foreach ($childDirectory in $childDirectories) {
@@ -15,9 +25,13 @@ while ($directoriesToVisit.Count -gt 0) {
 
         if ($artifactDirectoryNames -contains $childDirectory.Name) {
             Remove-Item -LiteralPath $childDirectory.FullName -Force -Recurse
+            $deletedDirectoryCount++
+            Write-Host ("Deleted {0}: {1}" -f $deletedDirectoryCount, $childDirectory.FullName)
             continue
         }
 
         $directoriesToVisit.Push($childDirectory.FullName)
     }
 }
+
+Write-Host ("Finished. Scanned {0} directories; deleted {1} artifact folders." -f $scannedDirectoryCount, $deletedDirectoryCount)
